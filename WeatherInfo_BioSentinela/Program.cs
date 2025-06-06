6e92042159ba94aea1d3182e32401490d7b8f0f0
davidbryan20 <davidbryan20@icloud.com>
2025-06-06 00:02:34 -0300

    Feito.

---
+++ WeatherInfo_BioSentinela/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona serviços ao container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configura o pipeline HTTP
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication(); 
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Login}/{id?}");
+
+app.Run();
